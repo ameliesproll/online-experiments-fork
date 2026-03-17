b135a01ee119a8095771456f63a8ccfa666cdcde
ameliesproll <as.diverses@gmail.com>
2026-03-17 10:43:35 +0100

    Exercise 5.1.4

--- Session5/amelie/session5.js
+++ Session5/amelie/session5.js
@@ -1,30 +1,27 @@
-//let ageGroup;
-//const age = 20;
-//if(age<18){
-//    ageGroup = "Child";
-//
-//}else{
-//    ageGroup = "Adult";
-//}
+/*Exercise 5.1.1
+
+let ageGroup;
+const age = 20;
+if(age<18){
+    ageGroup = "Child";
+
+}else{
+    ageGroup = "Adult";
+}
 
 //console.log("ageGroup:",ageGroup);
+*/
 
+
+
+
+//Exercise 5.1.2
 
 let age = 18;
 let ageGroup = age<18?"child":"adult";
+console.log("ageGroup:", ageGroup);
 
-let reactionTime = 100;
-switch (true) {
-case reactionTime < 200 && reactionTime > 0:
-console.log("Fast Response! Excellent!");
-break;
-case reactionTime <= 500 && reactionTime >= 200
-:
-console.log("Moderate Response! Keep it up!");
-break;
-case reactionTime > 500:
-console.log("Slow Response. Try to improve!");
-break;
-default:
-console.log("Invalid reaction time.");
+
+for(let i=0; i < 5; i++){
+    console.log("i/number:", i+1);
 }
