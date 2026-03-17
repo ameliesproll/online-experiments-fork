61f4b68e19de19daaa60b6738154a8e4654dce65
ameliesproll <as.diverses@gmail.com>
2026-03-17 10:33:06 +0100

    Exercises 5.1.2 & 5.1.3

--- Session5/amelie/session5.js
+++ Session5/amelie/session5.js
@@ -1,10 +1,30 @@
-let ageGroup;
-const age = 20;
-if(age<18){
-    ageGroup = "Child";
+//let ageGroup;
+//const age = 20;
+//if(age<18){
+//    ageGroup = "Child";
+//
+//}else{
+//    ageGroup = "Adult";
+//}
 
-}else{
-    ageGroup = "Adult";
+//console.log("ageGroup:",ageGroup);
+
+
+let age = 18;
+let ageGroup = age<18?"child":"adult";
+
+let reactionTime = 100;
+switch (true) {
+case reactionTime < 200 && reactionTime > 0:
+console.log("Fast Response! Excellent!");
+break;
+case reactionTime <= 500 && reactionTime >= 200
+:
+console.log("Moderate Response! Keep it up!");
+break;
+case reactionTime > 500:
+console.log("Slow Response. Try to improve!");
+break;
+default:
+console.log("Invalid reaction time.");
 }
-
-console.log("ageGroup:",ageGroup);
